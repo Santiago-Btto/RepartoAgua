35396936550e7d134e21c9c910ce0e04ca805023
Santiago <santiagobtto30@gmail.com>
2025-11-05 00:49:54 -0300

    +fixies

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCJ3l5q1QNkz5aIKZNhhSgLoiriYdm7hoo",
+export const firebaseConfig = {
+  apiKey: "AIzaSyC31Sgl0N5ka5lZXNhHsl9loirYhDm7hoo",
   authDomain: "repartoagua-9fa53.firebaseapp.com",
   projectId: "repartoagua-9fa53",
   storageBucket: "repartoagua-9fa53.firebasestorage.app",
   messagingSenderId: "1074698000415",
-  appId: "1:1074698000415:web:7d32aff99c37362fd01d2a",
-  measurementId: "G-972M7DQDXD"
+  appId: "1:1074698000415:web:7d32aff99c37362fd201d2a",
+  measurementId: "G-972MDYQQXD"
 };
--- sw.js
+++ sw.js
@@ -1,23 +1,39 @@
-const CACHE = 'agua-nologin-v3.2';
-const SHELL = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest'];
+// sw.js
+const CACHE = 'agua-nologin-fix-v1';
+const SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest'
+];
 
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
-self.addEventListener('fetch', e => {
+
+self.addEventListener('fetch', (e) => {
+  // Ignorar todo lo que no sea http/https (ej: chrome-extension://)
   if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.protocol !== 'http:' && url.protocol !== 'https:') return;
+
   e.respondWith(
-    caches.match(e.request).then(c =>
-      c || fetch(e.request).then(r => {
-        const rc = r.clone();
-        caches.open(CACHE).then(cc => cc.put(e.request, rc));
-        return r;
-      }).catch(() => c)
+    caches.match(e.request).then(cached =>
+      cached ||
+      fetch(e.request).then(res => {
+        const resClone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, resClone));
+        return res;
+      }).catch(() => cached)
     )
   );
 });
