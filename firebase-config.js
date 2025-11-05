cb119bcd1403afbfd16a7d9b18f76704af42f3a7
Santiago <santiagobtto30@gmail.com>
2025-11-05 00:20:20 -0300

    +firebase-config

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-// Pegá tu configuración aquí:
-export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "tu-app.firebaseapp.com",
-  projectId: "tu-app",
-  storageBucket: "tu-app.appspot.com",
-  messagingSenderId: "000000000000",
-  appId: "1:000000000000:web:XXXXXXXXXXXXXX"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ3l5q1QNkz5aIKZNhhSgLoiriYdm7hoo",
+  authDomain: "repartoagua-9fa53.firebaseapp.com",
+  projectId: "repartoagua-9fa53",
+  storageBucket: "repartoagua-9fa53.firebasestorage.app",
+  messagingSenderId: "1074698000415",
+  appId: "1:1074698000415:web:7d32aff99c37362fd01d2a",
+  measurementId: "G-972M7DQDXD"
 };
